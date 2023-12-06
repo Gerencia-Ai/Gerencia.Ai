63ea83cca0c7d89f032978fb479fff8e58c50b1a
snow-sr <joaofelipi877@gmail.com>
2023-12-06 16:24:48 -0300

    TESTE URGENTE

--- src/plugins/api/api.js
+++ src/plugins/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://gerencia-back-7ap3-dev.fl0.io/api/",
+  baseURL: "https://gerencia-back-dev-kbzk.4.us-1.fl0.io/api/",
 });
 
 export default api;
